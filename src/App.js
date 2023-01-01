b1f7f85155d78210081a1bdc8807d024632e0ebe
Sidharth Mahala <sidharthmahala@gmail.com>
2023-01-01 15:02:21 +0530

    Removed bugs files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import VirtualCard from './VirtualCard';
-import MaskNumbers from './MaskNumbers';
 
 function App() {
   return (
     <div>
       <h1>Virtual Card</h1>
       <VirtualCard />
-      <MaskNumbers />
     </div>
   );
 }
